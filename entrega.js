6d2660d9e81c179220c7497ee52e53598f4fdd94
oscarrodriguez <oscarfeliperodriguez21@gmail.com>
2023-01-15 20:42:22 -0500

    vamosquevamos

---
+++ entrega.js
@@ -0,0 +1,76 @@
+const fs = require ('fs');
+const { findSourceMap } = require('module');
+const { json } = require('node:stream/consumers');
+const { join } = require('path');
+
+const datos = './datos.json';
+
+class ProductManager{
+    
+async addProduct(producto){
+    try {
+        const datosProductos = await this.getProduct()
+        datosProductos.push(productos)
+        await fs.promises.writeFile(producto,JSON.stringify(datosProductos))
+    } catch (error) {
+        
+    }
+}
+
+async getProduct(){
+    try {
+        if (fs.existsSync(datos)){
+            const productos = await fs.promises.readFile(datos,'utf-8')
+            const productosJS = JSON.parse(productos)
+            return productosJS
+        }else{
+            return []
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async getProductById(){
+    try {
+        if (fs.existsSync(id)){
+            const productoId = await fs.identify(id)
+        }else{
+            return ('el producto se encuentra')
+        }
+    } catch (error) {
+        console.log('el producto no se encuentra')
+    }
+}
+
+async getDelete(){
+    try {
+        if (fs.existsSync(id)){
+            const borrarProducto = await fs.unlink(id)
+        }else{
+            return ('el producto se encuentra')
+        }
+    } catch (error) {
+        console.log('el producto no se encuentra')
+    }
+}
+
+
+}
+
+const producto = new ProductManager()
+
+async function entrega () {
+const getProduct = await producto.getProduct()
+console.log(getProduct)
+
+const getProductById = await getProductById.productoId
+console.log(getProductById)
+
+const getDelete = await getProductById.productoId
+console.log (getDelete)
+}
+
+
+
+entrega()
--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-const { profileEnd } = require('console')
+
 const fs = require('fs')
 const { json } = require('node:stream/consumers')
 const datos = './datos.json'
 
 class ProductManager{
     constructor (){
-        this.productos=[]
+        this.productos=[datos]
     } 
+
 }
 
-    async getProductos (productos) {
+
+    getProductos (datos) {
         try {
             const dataproductos = await this.consultarProductos()
-            dataproductos = push(productos)
+            dataproductos = push(datos)
         } catch (error) {
             
         }
     }
 
 
-    async consultarProductos(){
+    consultarProductos(){
         try {
             if (fs.existsSync(datos)){
                 const productos = await fs.promises.readFile(datos, id)
@@ -31,11 +33,21 @@ class ProductManager{
         } catch (error) {
             console.log(error)
         }
+
+        #agregarId(){
+            let id = 1
+            if (this.datos.length!==0){
+                id = this.datos[this.datos.length-1].id+1
+            }
+            return id
+        }
+        
     }
 
-    const productos1= new ProductManager()
+    const productos = new ProductManager()
 
     async function entrega2 (){
         const consultarProductos = await entrega2.consultarProductos()
         console.log(consultarProductos)
     }
+
